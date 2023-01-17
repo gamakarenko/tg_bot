4adad3b00f7e30eb4f0c25079a1fa00fbd377957
Vladislav Kulyakin <kulyakinv@gmail.com>
2023-01-17 15:21:16 +0300

    deploy test3

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 const router = express.Router();
+const home = require("./routes/home");
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '5741365706:AAF_9pixhfXSGu64g7oQbVrAwZjQUOUePeU';
 const webAppUrl = 'https://final-final.vercel.app/'
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
 
@@ -16,23 +15,14 @@ app.use(express.json());
 app.use(cors());
 
 
-// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const resp = match[1];
 
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 
 });
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -54,8 +44,7 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, JSON.stringify(data))
 }
 });
-router.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+
+app.use("/home", home);
 
 app.listen(PORT, () => console.log('server started on PORT ' + PORT))
