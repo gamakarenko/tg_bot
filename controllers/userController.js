dd36e78ac2c7c65cec0d959116abead9e84bd599
Kulyakin Vladislav <kulyakinv@gmail.com>
2023-01-20 11:45:25 +0300

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,6 @@ const { User, Passenger, Order } = require('../models/models')
 
 
 class UserController {
-    async registration(req, res, next) {
-        const { email, telegramId, role } = req.body
-        const user = await User.create({ email, role, telegramId })
-        return res.json({ message: 'Пользователь создан' })
-    }
-    async getAll(req, res) {
-        const brands = await User.findAll()
-        return res.json(brands)
-    }
     async getTransfers(req, res) {
         const {id} = req.param
         const transfers = await Order.findAll({
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/userController')
 
-router.post('/reg', userController.registration)
 router.post('/create/:id', userController.createTransfer)
-router.get('/reg', userController.getAll)
 router.get('/transfers/:id', userController.getTransfers)
 router.get('/transfer/:id', userController.getTransferById)
 //router.post('/login', userController.login)
