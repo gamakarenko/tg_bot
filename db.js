d29dc49704ca7c2527019cef4e2d1355c40c0537
FedAG <gleb.makarenko.2022@yandex.ru>
2023-02-06 00:27:22 +0300

    fix4

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require("sequelize")
 module.exports = new Sequelize(
     "vqjhduqs", // Название базы данных
     'vqjhduqs', // Пользователь
-    '4oh5JKhdbaFBYy53x5wpxQCpuUojFX8K', // Пароль
+    'Mchm77xSjDB39xHnfRaL0BmIVIrexBTO', // Пароль
     {
         dialect: 'postgres',
         host: 'hattie.db.elephantsql.com',
