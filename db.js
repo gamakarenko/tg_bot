e435a2e4fee63ac8b7ce17869cf269826e35bd23
FedAG <gleb.makarenko.2022@yandex.ru>
2023-02-05 19:41:07 +0300

    fix1

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ require('pg')
 const { Sequelize } = require("sequelize")
 
 module.exports = new Sequelize(
-    "ouodzzyy", // Название базы данных
-    'ouodzzyy', // Пользователь
-    'Shsc92QRXjoqNwaOaPTZQR_kOBaRxalV', // Пароль
+    "vqjhduqs", // Название базы данных
+    'vqjhduqs', // Пользователь
+    '1b_Wcx9hGaw7a4QvrC0RZ6WkYCS80YO2', // Пароль
     {
         dialect: 'postgres',
         host: 'dumbo.db.elephantsql.com',
